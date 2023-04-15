2d7f93bd649fd7d326217ae4c665a7d8651763e0
OliPet <web.oliverpeter@gmail.com>
2023-04-15 22:17:58 +0200

    improve meme container and image html&css / implement click event handler on button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,30 @@
 import React from "react";
+import Memes from "../memesData.js"
+
 
 export default function Main() {
+
+  const items = Memes.data.memes
+  let meme;
+
+function getRndMeme() {
+    const rnd = Math.floor(Math.random() * items.length);
+    meme = items[rnd].url;
+  }
+
+  function handleClick() {
+    console.log("clicked")
+  }
+
+ 
   return (
     <section className="main">
       <input type="text" className="memeProp1 propInput"></input>
       <input type="text" className="memeProp2 propInput"></input>
-      <input type="button" className="getMeme" value="Get a new meme image  🖼"></input>
-      <div className="memeContainer"></div>
+      <input type="button" onClick={getRndMeme}className="getMeme" value="Get a new meme image  🖼"></input>
+      <div className="memeContainer">
+        <img src={meme} className="memeImage" ></img>
+      </div>
     </section>
   )
 }
